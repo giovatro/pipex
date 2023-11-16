a64e0f5a994d32e56c46a9f1bb1759ee3dc2b519
Giovanni Troiano <136205893+giovatro@users.noreply.github.com>
2023-11-16 17:35:01 +1030

    Add files via upload

--- parse.c
+++ parse.c
@@ -6,25 +6,26 @@
 /*   By: gtroiano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 14:11:56 by gtroiano          #+#    #+#             */
-/*   Updated: 2023/11/14 14:12:02 by gtroiano         ###   ########.fr       */
+/*   Updated: 2023/11/16 17:25:43 by gtroiano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
 
-char **get_path(char **envp)
+char	**get_path(char **envp)
 {
-    char **env_paths;
-    char *tmp;
-    int i;
+	char	**env_paths;
+	char	*tmp;
+	int		i;
 
-    i = 0;
-    while (envp[i] && ft_strncmp(envp[i], "PATH=", 5))
-        i++;
-    if (!envp[i])
-        return (NULL);
-    tmp = envp[i] + 5;
-    env_paths = ft_split(tmp, ':');
-    i = 0;
+	i = 0;
+	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5))
+		i++;
+	if (!envp[i])
+		return (NULL);
+	tmp = envp[i] + 5;
+	env_paths = ft_split(tmp, ':');
+	i = 0;
 	while (env_paths[i])
 	{
 		tmp = env_paths[i];
@@ -34,39 +35,35 @@ char **get_path(char **envp)
 	}
 	return (env_paths);
 }
-    
 
+char	*find_cmd_path(char *cmd, char **path)
+{
+	char	*cmd_path;
+	int		i;
+
+	i = 0;
+	path = get_path(path);
+	while (path[i])
+	{
+		cmd_path = ft_strjoin(path[i], cmd);
+		if (access(cmd_path, F_OK | X_OK) == 0)
+			return (cmd_path);
+		free(cmd_path);
+		i++;
+	}
+	return (NULL);
 }
 
-char    *find_cmd_path(char *cmd, char **path)
+void	execute_cmd(char *cmd, char **envp)
 {
-    char    *cmd_path;
-    int i;
+	char	**args;
+	char	*pathname;
 
-    i = 0;
-    path = get_path(path);
-    while(path[i])
-    {
-        cmd_path = ft_strjoin(path[i], cmd);
-        if (access(cmd_path, F_OK | X_OK) == 0)
-            return (cmd_path);
-        free(cmd_path);
-        i++;
-    }
-    return (NULL);
-}
-
-
-void    execute_cmd(char **cmd, char **envp)
-{
-    char **args;
-    char *exe_path;
-
-    args = ft_split(cmd, ' ');
-    exe_path = find_cmd_path(args[0], envp);
-    execve(exe_path, args, envp);
-    write(2, "pipex: ", 7);
-    write(2, "command not found: ", 20);
-    write(2, cmd, ft_strlen(cmd);
-    exit(127);
+	args = ft_split(cmd, ' ');
+	pathname = find_cmd_path(args[0], envp);
+	execve(pathname, args, envp);
+	write(2, "pipex: ", 7);
+	write(2, "command not found: ", 20);
+	write(2, cmd, ft_strlen(cmd));
+	exit(127);
 }
